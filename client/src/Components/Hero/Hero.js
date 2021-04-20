d9530d25d07f93bd1c8a91eee8bf2572fe436f9c
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-04-20 22:46:55 +0600

    Responsive hero done

--- client/src/Components/Hero/Hero.js
+++ client/src/Components/Hero/Hero.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import HeroImg from "../../Assets/Hero/heroImg.png";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
-    <Container fluid className={styles.hero}>
-      <Row>
-        <Col>AAA</Col>
-      </Row>
-    </Container>
+    <div className={styles.hero}>
+      <div className={`${styles.left} my-auto`}>
+        <div className="pb-5">
+          <h1 className="heading__1">Cover Generator</h1>
+          <span className="d-block heading__4 pb-5">
+            Easiest way to make your cover pages
+          </span>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <img src={HeroImg} alt="hero" />
+      </div>
+    </div>
   );
 };
 
