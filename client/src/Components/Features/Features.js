57e6b6327065e691b6997543c8bda822625a75ef
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-04-22 21:20:16 +0600

    small bugs fixed

--- client/src/Components/Features/Features.js
+++ client/src/Components/Features/Features.js
@@ -7,11 +7,12 @@ import data from "./stub/fakeData";
 const Features = () => {
   return (
     <div className={styles.features}>
-      <span className="d-block heading__2 text-center">Our Features</span>
-      <span className="d-block lead__1 text-center">
-        We love making your life easy by providing useful and effective features
-      </span>
       <Container>
+        <span className="d-block heading__2 text-center">Our Features</span>
+        <span className="d-block lead__1 text-center">
+          We love making your life easy by providing useful and effective
+          features
+        </span>
         <Row className="py-5">
           {data.map((feature) => (
             <FeatureItem {...feature} key={feature.id} />
