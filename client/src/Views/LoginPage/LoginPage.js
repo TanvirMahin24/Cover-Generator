433a0445fd28dce71b7e8075c58964456c91df5d
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-04-23 20:50:55 +0600

    Login page responsive done

--- client/src/Views/LoginPage/LoginPage.js
+++ client/src/Views/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Footer } from "../../Components/Footer";
 import Login from "../../Components/Login/Login";
 import Navbar from "../../Components/Navbar/Navbar";
 
@@ -7,6 +8,7 @@ const LoginPage = () => {
     <>
       <Navbar />
       <Login />
+      <Footer />
     </>
   );
 };
