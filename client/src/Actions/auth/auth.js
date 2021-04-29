a25c1f053c3c5eca6616b99f0140d40accdc6575
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-04-29 11:24:28 +0600

    auth token util added

---
+++ client/src/Actions/auth/auth.js
@@ -0,0 +1,33 @@
+import {
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  CLEAR_PROFILE,
+} from "../../Constants/Types/Types";
+import setAlert from "../alert/alert";
+import axios from "axios";
+import setAuthToken from "../../utils/setAuthToken";
+
+//Load user
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get("/api/auth");
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
